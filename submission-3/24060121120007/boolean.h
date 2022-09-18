729b81f4758b2f2eb7e356450e41000f15562888
IvanSHarianja <100180168+IvanSHarianja@users.noreply.github.com>
2022-09-18 12:14:32 +0700

    Add files via upload

---
+++ submission-3/24060121120007/boolean.h
@@ -0,0 +1,14 @@
+#ifndef boolean_H
+#define boolean_H
+/* Program   : boolean.h */
+/* Deskripsi : Memberikan defenisi boolean*/
+/* NIM/Nama  : 24060121120007/Ivan S Harianja*/
+/* Tanggal   : 11 september 2022*/
+/***********************************/
+
+//type boolean macro bahasa C, false=0, true=1
+#define false 0
+#define true  1
+#define boolean unsigned char
+
+#endif
